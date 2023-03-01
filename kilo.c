d8fcc2b5c3ded88664c4341dba38afb59f9807ea
rqda <83322694+rqdaA@users.noreply.github.com>
2023-03-02 01:09:18 +0900

    add termio

--- kilo.c
+++ kilo.c
@@ -1,1 +1,18 @@
-int main() { return 0; }
+#include <termio.h>
+#include <unistd.h>
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_cflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    ;
+  return 0;
+}
