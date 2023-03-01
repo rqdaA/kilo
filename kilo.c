8a1802e724ce49aa3145606a5390e582a851432f
rqda <83322694+rqdaA@users.noreply.github.com>
2023-03-02 02:54:07 +0900

    show input

--- kilo.c
+++ kilo.c
@@ -1,10 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termio.h>
 #include <unistd.h>
 
+struct termios orig_terminos;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminos); }
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_cflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_terminos);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_terminos;
+  raw.c_iflag &= ~(IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -12,7 +22,11 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c))
+      printf("%d\n", c);
+    else
+      printf("%d ('%c')\n", c, c);
+  }
   return 0;
 }
