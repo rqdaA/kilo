0332b3df2af37edb66c491f886786d78fa735655
rqda <83322694+rqdaA@users.noreply.github.com>
2023-03-07 20:15:27 +0900

    add refresh screen

--- kilo.c
+++ kilo.c
@@ -6,26 +6,35 @@
 #include <termio.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k)&0x1f)
+
 /*** data ***/
-struct termios orig_terminos;
+struct editorConfig {
+  struct termios orig_terminos;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminos) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_terminos) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_terminos) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_terminos) == -1)
     die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_terminos;
+  struct termios raw = E.orig_terminos;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag &= (CS8);
@@ -35,20 +44,52 @@ void enableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("READ");
+  }
+  return c;
+}
+
+/*** output ***/
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4); // Clear Screen
+  write(STDOUT_FILENO, "\x1b[H", 3);  // Set Mouse Pos 1:1
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3); // Set Mouse Pos 1:1
+}
+
+/*** input ***/
+void editorProcessKeyPress() {
+  char c = editorReadKey();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    exit(0);
+    break;
+  }
+}
+
 /*** init ***/
 int main() {
   enableRawMode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-      die("READ");
-    if (iscntrl(c))
-      printf("%x\r\n", c);
-    else
-      printf("%x ('%c')\r\n", c, c);
-    if (c == 'q')
-      break;
+    editorRefreshScreen();
+    editorProcessKeyPress();
   }
   return 0;
 }
